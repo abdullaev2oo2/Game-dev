a1277f4b809b990c79c0cc748bd9982e163ccf25
abdullaev2oo2 <sarvar.bek.790@gmail.com>
2022-10-28 03:12:04 +0500

    Tilemanager | auto add tiles and optimization

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public GameObject[] tilePrefabs;
+    public float zSpwan = 0;
+    public float tileLength = 30;
+    public int numberOfTils = 5;
+    private List<GameObject> activeTiles = new List<GameObject>();
+    public Transform playerTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numberOfTils; i++)
+        {
+            if (i == 0) SpawnTile(0);
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerTransform.position.z - 35 > zSpwan - (numberOfTils * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject go =  Instantiate(tilePrefabs[tileIndex], transform.forward * zSpwan, transform.rotation);
+        activeTiles.Add(go);
+        zSpwan += tileLength;
+    }
+}
