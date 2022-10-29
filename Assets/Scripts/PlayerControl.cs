86318619d60e3947b776f7476ed6c62e8bcaceac
abdullaev2oo2 <sarvar.bek.790@gmail.com>
2022-10-29 15:10:43 +0500

    animation fixed

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,113 +5,159 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    private CharacterController characterController;
-    private Vector3 direction;
-    public float Speed;
+    private CharacterController controller;
+    private Vector3 move;
+    public float forwardSpeed;
     public float maxSpeed;
 
-    private int desiredLine = 1;
-    public float laneDistance = 2.5f;
-
-    public float jumForce;
-    public float gravity = -20;
-    public Animator animator;
+    private int desiredLane = 1;//0:left, 1:middle, 2:right
+    public float laneDistance = 2.5f;//The distance between tow lanes
 
     public bool isGrounded;
     public LayerMask groundLayer;
     public Transform groundCheck;
 
-    public bool isSliding = false;
+    public float gravity = -12f;
+    public float jumpHeight = 2;
+    private Vector3 velocity;
 
-    // Start is called before the first frame update
+    public Animator animator;
+    private bool isSliding = false;
+
+    public float slideDuration = 1.5f;
+
+    bool toggle = false;
+
     void Start()
     {
-        characterController = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
+        Time.timeScale = 1.2f;
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (!PlayerManager.isGameStarted || PlayerManager.gameOver)
+            return;
+
+        //Increase Speed
+        if (toggle)
+        {
+            toggle = false;
+            if (forwardSpeed < maxSpeed)
+                forwardSpeed += 0.1f * Time.fixedDeltaTime;
+        }
+        else
+        {
+            toggle = true;
+            if (Time.timeScale < 2f)
+                Time.timeScale += 0.005f * Time.fixedDeltaTime;
+        }
+    }
+
     void Update()
     {
-        if (!PlayerManager.isGameStarted) return;
-
-        characterController.Move(direction * Time.deltaTime);
-
-        if(Speed < maxSpeed)
-        {
-            Speed += 0.1f * Time.deltaTime;
-        }
+        if (!PlayerManager.isGameStarted || PlayerManager.gameOver)
+            return;
 
         animator.SetBool("isGameStarted", true);
-        animator.SetBool("isGrounded", isGrounded);
-        direction.z = Speed;
+        move.z = forwardSpeed;
 
         isGrounded = Physics.CheckSphere(groundCheck.position, 0.17f, groundLayer);
+        animator.SetBool("isGrounded", isGrounded);
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -1f;
 
         if (isGrounded)
         {
-            //direction.y = -1;
-            if(SwipeManager.swipeUp) {
+            if (SwipeManager.swipeUp)
                 Jump();
+
+            if (SwipeManager.swipeDown && !isSliding)
+                StartCoroutine(Slide());
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+            if (SwipeManager.swipeDown && !isSliding)
+            {
+                StartCoroutine(Slide());
+                velocity.y = -10;
             }
-        }else
+
+        }
+        controller.Move(velocity * Time.deltaTime);
+
+        //Gather the inputs on which lane we should be
+        if (SwipeManager.swipeRight)
         {
-            direction.y += gravity * Time.deltaTime;
+            desiredLane++;
+            if (desiredLane == 3)
+                desiredLane = 2;
+        }
+        if (SwipeManager.swipeLeft)
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
         }
 
-        if (SwipeManager.swipeDown && !isSliding)
+        //Calculate where we should be in the future
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (desiredLane == 0)
+            targetPosition += Vector3.left * laneDistance;
+        else if (desiredLane == 2)
+            targetPosition += Vector3.right * laneDistance;
+
+        //transform.position = targetPosition;
+        if (transform.position != targetPosition)
         {
-            StartCoroutine(Slide());
+            Vector3 diff = targetPosition - transform.position;
+            Vector3 moveDir = diff.normalized * 30 * Time.deltaTime;
+            if (moveDir.sqrMagnitude < diff.magnitude)
+                controller.Move(moveDir);
+            else
+                controller.Move(diff);
         }
 
+        controller.Move(move * Time.deltaTime);
+    }
 
-        if (SwipeManager.swipeRight && desiredLine != 2)
+    private void Jump()
+    {
+        StopCoroutine(Slide());
+        animator.SetBool("isSliding", false);
+        //animator.SetTrigger("jump");
+        controller.center = Vector3.zero;
+        controller.height = 2;
+        isSliding = false;
+
+        velocity.y = Mathf.Sqrt(jumpHeight * 2 * -gravity);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.transform.tag == "Obstacle")
         {
-            desiredLine++;
+            PlayerManager.gameOver = true;
+            //FindObjectOfType<AudioManager>().PlaySound("GameOver");
         }
-
-        if (SwipeManager.swipeLeft && desiredLine != 0)
-        {
-            desiredLine--;
-        }
-
-        Vector3 targerPositon = transform.position.z * transform.forward + transform.position.y * transform.up;
-
-        if(desiredLine == 0)
-        {
-            targerPositon += Vector3.left * laneDistance;
-        }else if (desiredLine == 2)
-        {
-            targerPositon += Vector3.right * laneDistance;
-        }
-
-        transform.position = Vector3.Lerp(transform.position, targerPositon, 80*Time.deltaTime);
     }
 
     private IEnumerator Slide()
     {
         isSliding = true;
         animator.SetBool("isSliding", true);
-        characterController.center = new Vector3(0, -0.15f, 0);
-        characterController.height = 1;
+        yield return new WaitForSeconds(0.25f / Time.timeScale);
+        controller.center = new Vector3(0, -0.5f, 0);
+        controller.height = 1;
 
-        yield return new WaitForSeconds(1.3f);
+        yield return new WaitForSeconds((slideDuration - 0.25f) / Time.timeScale);
 
         animator.SetBool("isSliding", false);
-        characterController.center = new Vector3(0, 0, 0);
-        characterController.height = 2;
+
+        controller.center = Vector3.zero;
+        controller.height = 2;
+
         isSliding = false;
-    }
-
-    private void Jump()
-    {
-        direction.y = jumForce;        
-    }
-
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if(hit.transform.tag == "Obstacle")
-        {
-            PlayerManager.gameOver = true;
-        }
     }
 }
